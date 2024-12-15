17123005b3ba4631d83d2568c90465ba1b63245f
Nimesh sajith kumara <sajithkumaranimesh@gmail.com>
2024-12-15 12:41:03 +0530

    setup environment

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: path_1.default.resolve(__dirname, '../config.env') });
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Express running port :${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import path from 'path';
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, '../config.env') });
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Express running port :${PORT}`);
