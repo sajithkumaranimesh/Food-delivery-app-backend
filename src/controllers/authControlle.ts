cc269cbeaf478cf2d742ba0057505e2e86bbeacb
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-12-15 21:13:36 +0530

    create catchAsync function for the reusing try catch block

--- src/controllers/authControlle.ts
+++ src/controllers/authControlle.ts
@@ -1,59 +1,53 @@
 import { NextFunction, Request, Response } from "express";
-import User from "../models/userModel"
+import User from "../models/userModel";
 import { IUser } from "../models/userModel";
 import jwt from "jsonwebtoken";
+import AppError from "../utils/appError";
+import { catchAsync } from "../utils/catchAsync";
 
-
-const signToken = (id:unknown) => {
-    const secret_key = process.env.JWT_SERET || "secret_key"
+const signToken = (id: unknown) => {
+    const secret_key = process.env.JWT_SERET || "secret_key";
     return jwt.sign({ id }, secret_key, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    })
-}
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
 
-export const signup = async (req: Request<{}, {}, IUser>, res: Response, next: NextFunction) => {
-    try {
+export const signup = catchAsync(
+    async (req: Request<{}, {}, IUser>, res: Response, next: NextFunction) => {
         const newUser: IUser = await User.create({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password,
-            passwordConfirm: req.body.passwordConfirm
-        })
+            passwordConfirm: req.body.passwordConfirm,
+        });
         const token = signToken(newUser._id);
 
         res.status(201).json({
             status: "success",
-            token: token
+            token: token,
         });
-
-    } catch (err) {
-        return next(err);
     }
-    next();
-}
+);
 
-export const login = async (req:Request, res:Response, next:NextFunction) => {
-    try{
+export const login = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
         const { email, password } = req.body;
 
         if (!email || !password) {
-            return next(console.log("Please provide email and password!"))
+            return next(new AppError("Please provide email and password!", 400));
         }
 
-        const user = await User.findOne({ email }).select('+password');
+        const user = await User.findOne({ email }).select("+password");
 
         if (!user || !(await user.correctPassword(password, user.password))) {
-            return next(console.log("Incorrect email or password"))
+            return next(new AppError("Incorrect email or password", 401));
         }
 
         const token = signToken(user._id);
 
         res.status(201).json({
             status: "success",
-            token: token
+            token: token,
         });
-    }catch(err){
-        next(err);
     }
-    next();
-}
+);
