b2a74892064e47d5f683a14747ddad06e0cb8ffb
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-12-19 13:36:33 +0530

    create cart item controller

---
+++ src/controllers/cartController.ts
@@ -0,0 +1,83 @@
+import { Request, Response, NextFunction } from "express";
+import { CartItemDto } from "../dto/CartItemDto";
+import * as cartService from "../service/cartService";
+
+
+export const persist = async (req: Request<{},{},CartItemDto>, res: Response, next: NextFunction) => {
+    try{
+        const newCartItem = {
+            itemID: req.body.itemID,
+            quantity: req.body.quantity,
+            price: req.body.price,
+            totalPrice: req.body.totalPrice,
+            cartID: req.body.cartID
+        }
+        const savedCartItem = await cartService.persist(newCartItem);
+        res.status(201).json({
+            status: "success",
+            savedCartItem
+        });
+    }catch(err){
+        next(err);
+    }
+  
+}
+
+export const retrieve = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const cartItems = await cartService.retrieve();
+        res.status(200).json({
+            status: "success",
+            cartItems
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
+export const retrieveById = async (req: Request<{id: string}, {}, {}>, res: Response, next: NextFunction) => {
+    try{
+        const {id} = req.params;
+        const cartItem = await cartService.retrieveById(id);
+        res.status(200).json({
+            status: "success",
+            cartItem
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
+
+export const updateById = async (req: Request<{id: string},{},CartItemDto>, res: Response, next: NextFunction) => {
+    try{
+        const {id} = req.params;
+        const updatedCartItem = {
+            itemID: req.body.itemID,
+            quantity: req.body.quantity,
+            price: req.body.price,
+            totalPrice: req.body.totalPrice,
+            cartID: req.body.cartID
+        }
+        const cartItem = await cartService.updateById(id, updatedCartItem);
+        res.status(200).json({
+            status: "success",
+            cartItem
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
+export const deleteById = async (req: Request<{id: string}, {}, {}>, res: Response, next: NextFunction) => {
+    try{
+        const {id} = req.params;
+        const cartItem = await cartService.deleteById(id);
+        res.status(200).json({
+            status: "success",
+            cartItem
+        });
+    }catch(err){
+        next(err);
+    }
+}
